28746f8275739d3ed0175c7cbb4a3c6ea215a8c6
PepperMode <lookwithad@gmaol.com>
2026-09-05 13:23:30 +0200

    GEA-18 Add domain models

---
+++ src/test/java/com/gym/engagement/app/domain/Trainee.java
@@ -0,0 +1,11 @@
+package com.gym.engagement.app.domain;
+
+import java.time.LocalDate;
+
+public class Trainee {
+
+    private Long id;
+    private Long userId;
+    private LocalDate dateOfBirth;
+    private String address;
+}
---
+++ src/test/java/com/gym/engagement/app/domain/Training.java
@@ -0,0 +1,14 @@
+package com.gym.engagement.app.domain;
+
+import java.time.LocalDate;
+
+public class Training {
+
+    private Long id;
+    private Long traineeId;
+    private Long trainerId;
+    private String trainingName;
+    private Long trainingTypeId;
+    private LocalDate trainingDate;
+    private Integer trainingDuration;
+}
